15b61716e89c93fb555b8bd34e381ae49a40d8c1
Aditya Agnihotri <agnihotria700@gmail.com>
2026-01-05 21:39:55 +0530

    policy added to S3 bucket resource

--- Terraform/app/S3.tf
+++ Terraform/app/S3.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "static_website" {
   bucket = var.bucket_name  
 }
+
+resource "aws_s3_bucket_website_configuration" "static_website_config" {
+  bucket = aws_s3_bucket.static_website.bucket.id
+
+
+  index_document {
+    suffix = "index.html"
+  }  
+}
