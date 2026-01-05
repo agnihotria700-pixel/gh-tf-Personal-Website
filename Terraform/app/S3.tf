088ace8625d92b12b3693272b070cfc15a1c4d80
Aditya Agnihotri <agnihotria700@gmail.com>
2026-01-05 21:55:14 +0530

    error updated

--- Terraform/app/S3.tf
+++ Terraform/app/S3.tf
@@ -3,13 +3,23 @@ resource "aws_s3_bucket" "static_website" {
 }
 
 resource "aws_s3_bucket_website_configuration" "static_website_config" {
-  bucket = aws_s3_bucket.static_website.bucket.id
+  bucket = aws_s3_bucket.static_website.id
 
 
   index_document {
     suffix = "index.html"
   }  
 }
+
+
+resource "aws_s3_bucket_public_access_block" "static_website_public_access" {
+    bucket = aws_s3_bucket.static_website.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+  }
 
 resource "aws_s3_bucket_policy" "static_website_policy" {
   bucket = aws_s3_bucket.static_website.id
@@ -26,4 +36,6 @@ resource "aws_s3_bucket_policy" "static_website_policy" {
       }
     ]
   })
+
+  depends_on = [ aws_s3_bucket_public_access_block.static_website_public_access ]
 }
