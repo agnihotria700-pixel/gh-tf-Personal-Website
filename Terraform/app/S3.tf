2273ffd49af047451cccea76f7e3649b7d34f104
Aditya Agnihotri <agnihotria700@gmail.com>
2026-01-05 21:44:04 +0530

    update S3 bucket configuration

--- Terraform/app/S3.tf
+++ Terraform/app/S3.tf
@@ -10,3 +10,20 @@ resource "aws_s3_bucket_website_configuration" "static_website_config" {
     suffix = "index.html"
   }  
 }
+
+resource "aws_s3_bucket_policy" "static_website_policy" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
